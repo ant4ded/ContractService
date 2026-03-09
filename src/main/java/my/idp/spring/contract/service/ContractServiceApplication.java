0cb84f40212674cd7a85b1f47f6601118aa7a409
Anton Dzedzik <adzedzik@ibagroup.eu>
2026-03-09 12:25:07 +0300

    add profiles & sync with starter

--- src/main/java/my/idp/spring/contract/service/ContractServiceApplication.java
+++ src/main/java/my/idp/spring/contract/service/ContractServiceApplication.java
@@ -1,13 +1,21 @@
 package my.idp.spring.contract.service;
 
+import my.idp.spring.currency.rate.starter.dto.CurrencyRateDto;
+import my.idp.spring.currency.rate.starter.service.CurrencyRateService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ContractServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContractServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ContractServiceApplication.class, args);
+		CurrencyRateService bean = context.getBean(CurrencyRateService.class);
+		CurrencyRateDto rate = bean.getRate("BYN", "USD", LocalDate.now());
+		System.out.println(rate);
 	}
 
 }
