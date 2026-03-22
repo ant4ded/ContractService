6f8dc58e0320189f4232eabb2a7d5d43bd1821e9
adzedzik <adzedzik@mdait.by>
2026-03-22 15:35:45 +0300

    add actuator

--- src/main/java/my/idp/spring/contract/service/ContractServiceApplication.java
+++ src/main/java/my/idp/spring/contract/service/ContractServiceApplication.java
@@ -1,21 +1,11 @@
 package my.idp.spring.contract.service;
 
-import my.idp.spring.currency.rate.starter.dto.CurrencyRateDto;
-import my.idp.spring.currency.rate.starter.service.CurrencyRateService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class ContractServiceApplication {
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(ContractServiceApplication.class, args);
-		CurrencyRateService bean = context.getBean(CurrencyRateService.class);
-		CurrencyRateDto rate = bean.getRate("EUR", "USD", LocalDate.now());
-		System.out.println(rate);
+		SpringApplication.run(ContractServiceApplication.class, args);
 	}
-
 }
